0e21db340124c1d1b9686ce734a683cf038e8946
Matthew Bunday <zenceph@gmail.com>
2014-09-08 12:26:09 -0400

    Add root property to seed.

--- seeds.js
+++ seeds.js
@@ -2,7 +2,7 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     Trees.remove({});
     if (Trees.find().count() == 0) {
-      tree = Tree.create({title: "Matthew Bunday"});
+      tree = Tree.create({title: "Matthew Bunday", root: true});
       tree.create_child({title: "Strength"});
       tree.create_child({title: "Skill"});
       tree.create_child({title: "Discernment"});
