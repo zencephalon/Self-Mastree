9589cfee7b4214f413eb18989f436a610ae6c2bf
Matthew Bunday <zenceph@gmail.com>
2014-09-08 15:16:36 -0400

    Add another level of seed data.

--- seeds.js
+++ seeds.js
@@ -3,7 +3,9 @@ if (Meteor.isServer) {
     Trees.remove({});
     if (Trees.find().count() == 0) {
       tree = Tree.create({title: "Matthew Bunday", root: true});
-      tree.create_child({title: "Strength"});
+      str_tree = tree.create_child({title: "Strength"});
+      str_tree.create_child({title: "Taijutsu"});
+      str_tree.create_child({title: "Bodyweight"});
       tree.create_child({title: "Skill"});
       tree.create_child({title: "Discernment"});
     }
