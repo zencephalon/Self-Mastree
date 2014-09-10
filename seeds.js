e45de31aa22c1bed2d02ec7f8dfa7c278d067111
Matthew Bunday <zenceph@gmail.com>
2014-09-10 17:09:38 -0400

    Refactor seed file

--- seeds.js
+++ seeds.js
@@ -1,13 +1,23 @@
+function dropData() {
+  Trees.remove({});
+  Meteor.users.remove({});
+}
+
+function seedData() {
+  uid = Accounts.createUser({email: "mkbunday@gmail.com", password: "zen"});
+  tree = Tree.create({title: "Matthew Bunday", root: true, uid: uid});
+  str_tree = tree.create_child({title: "Strength"});
+  str_tree.create_child({title: "Taijutsu"});
+  str_tree.create_child({title: "Bodyweight"});
+  tree.create_child({title: "Skill"});
+  tree.create_child({title: "Discernment"});
+}
+
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    Trees.remove({});
+    //dropData();
     if (Trees.find().count() == 0) {
-      tree = Tree.create({title: "Matthew Bunday", root: true});
-      str_tree = tree.create_child({title: "Strength"});
-      str_tree.create_child({title: "Taijutsu"});
-      str_tree.create_child({title: "Bodyweight"});
-      tree.create_child({title: "Skill"});
-      tree.create_child({title: "Discernment"});
+      seedData();
     }
   });
 }
