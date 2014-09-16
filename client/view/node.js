124ae67499b3b800eed9abbad8ea8d5a873106a0
Matthew Bunday <zenceph@gmail.com>
2014-09-16 11:40:08 -0400

    Use ViewNode.select to fix bug

--- client/view/node.js
+++ client/view/node.js
@@ -2,6 +2,6 @@ Template.node.events({
   'click': function (event) {
     event.stopPropagation();
     this.incCount(true);
-    Session.set("selected_node", this._id);
+    ViewNode.select(this._id);
   }
 });
