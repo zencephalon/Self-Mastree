8632ff46b7a10f6f70a0a07435631559d94fe836
Matthew Bunday <zenceph@gmail.com>
2014-09-20 11:45:34 -0400

    New seed data.

--- seeds.js
+++ seeds.js
@@ -6,11 +6,15 @@ function dropData() {
 function seedData() {
   uid = Accounts.createUser({email: "mkbunday@gmail.com", password: "zen"});
   tree = Tree.create({title: "Matthew Bunday", root: true, uid: uid});
-  str_tree = tree.createChild({title: "Strength"});
-  str_tree.createChild({title: "Taijutsu"});
-  str_tree.createChild({title: "Bodyweight"});
-  tree.createChild({title: "Skill"});
-  tree.createChild({title: "Discernment"});
+  ninpo = tree.createChild({title: "Ninpo"});
+  taijutsu = ninpo.createChild({title: "Taijutsu"});
+  taijutsu.createChild({title: "Ichi Monji no Kamae"});
+  tsuki = taijutsu.createChild({title: "Tsuki"});
+  tsuki.createChild({title: "from Ichimonji"});
+  tsuki.createChild({title: "from Shizen"});
+  kenjutsu = ninpo.createChild({title: "Kenjutsu"});
+  kenjutsu.createChild({title: "Koho Seigan Nuki"});
+  kenjutsu.createChild({title: "Tsuki with Ken"});
 }
 
 if (Meteor.isServer) {
