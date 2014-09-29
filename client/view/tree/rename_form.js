a4b29519e1a24a3a941ba23890be5f3e6ec10f5d
Matthew Bunday <zenceph@gmail.com>
2014-09-29 16:54:41 -0400

    Add autocomplete to rename form

--- client/view/tree/rename_form.js
+++ client/view/tree/rename_form.js
@@ -1,3 +1,30 @@
+Template.rename_form.settings = function() {
+  return {
+    position: "bottom",
+    limit: 5,
+    rules: [{
+      token: ':',
+      replacement: '',
+      end_token: '',
+      collection: Trees,
+      field: "title",
+      template: Template.qs_tree_display,
+      callback: function(doc, element) {
+        var tree = Tree.findOneByTitle($('#switcher').val());
+        tree.focus();
+        tree.unfoldUp();
+        $('#quickswitcher').hide();
+      }
+    }]
+  }
+
+}
+
+Template.rename_form.initial_value = function () {
+  tree = Tree.focused();
+  return tree.title;
+}
+
 Template.rename_form.events({
   'submit': function() {
     tree = Tree.focused();
