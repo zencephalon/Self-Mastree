e05f4edf0eafe28ec5be94bb2689060891491ed6
Matthew Bunday <zenceph@gmail.com>
2014-09-29 23:05:30 -0400

    Create form using autocomplete

--- client/view/tree/create_form.js
+++ client/view/tree/create_form.js
@@ -1,3 +1,30 @@
+Template.create_form.settings = function() {
+  return {
+    position: "bottom",
+    limit: 5,
+    rules: [{
+      token: '@',
+      replacement: '@',
+      end_token: ' ',
+      collection: Trees,
+      filter: {attr: {"$exists": true}},
+      field: "attr",
+      template: Template.qs_tree_display,
+      callback: function(doc, element) {
+      }
+    }, {
+      token: '#',
+      replacement: '#',
+      end_token: ' ',
+      collection: Trees,
+      filter: {hash: {"$exists": true}},
+      field: "hash",
+      template: Template.qs_tree_display,
+      callback: function(doc, element) {}
+    }]
+  }
+};
+
 Template.create_form.events({
   'submit': function (event) {
     event.preventDefault();
