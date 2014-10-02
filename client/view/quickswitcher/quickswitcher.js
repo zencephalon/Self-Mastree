979fe5a04d4d003485e54e606ce614f4b893b381
Matthew Bunday <zenceph@gmail.com>
2014-10-02 12:25:33 -0400

    Add filter to quickswitcher

--- client/view/quickswitcher/quickswitcher.js
+++ client/view/quickswitcher/quickswitcher.js
@@ -9,6 +9,7 @@ Template.quickswitcher.settings = function() {
       collection: Trees,
       field: "ref",
       matchAll: true,
+      filter: {archived: {"$ne": true}},
       template: Template.qs_tree_display,
       callback: function(doc, element) {
         var tree = Tree.findOne(doc._id);
