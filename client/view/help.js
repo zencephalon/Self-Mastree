aff4274fa52a423195f3982a530ff098d8692e31
Matthew Bunday <zenceph@gmail.com>
2014-10-04 13:36:24 -0400

    Show and hide help.

--- client/view/help.js
+++ client/view/help.js
@@ -3,3 +3,12 @@ Template.help.help = function() {
     return Meteor.user().profile.show_help;
   }
 }
+
+Template.help.events({
+  'click a#hide-help': function() {
+    Meteor.users.update({_id: Meteor.user()._id}, {"$set": {'profile.show_help': false}});
+  },
+  'click a#show-help': function() {
+    Meteor.users.update({_id: Meteor.user()._id}, {"$set": {'profile.show_help': true}});
+  }
+});
