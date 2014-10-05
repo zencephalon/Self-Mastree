0ff68ec39f2c42c458ce4bebb45072860fe5b63e
Matthew Bunday <zenceph@gmail.com>
2014-10-04 20:54:21 -0400

    Use multi true..

--- server/seeds.js
+++ server/seeds.js
@@ -24,7 +24,7 @@ if (Meteor.isServer) {
 
   Meteor.startup(function () {
     //Trees.update({"createdAt": {"$exists":false}}, {"$set":{createdAt: new Date()}}, {multi:true});
-    Trees.update({uid: "i3RsjMfwdDEAhLR9K"}, {"$set":{createdAt: new Date(2014, 9, 1)}});
+    Trees.update({uid: "i3RsjMfwdDEAhLR9K"}, {"$set":{createdAt: new Date(2014, 9, 1)}}, {multi:true});
     //dropData();
     if (Trees.find().count() == 0) {
       seedData();
