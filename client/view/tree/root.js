1436d994984aff8653cb56dfc565e661d2f8526f
Matthew Bunday <zenceph@gmail.com>
2014-10-23 15:03:59 -0400

    Fix deprecation warnings.

--- client/view/tree/root.js
+++ client/view/tree/root.js
@@ -1,10 +1,12 @@
-Template.root.root = function() {
-  if (Meteor.userId()) {
-    return Tree.findOne({root: true, uid: Meteor.userId()});
-  } else {
-    return {text: "Sign in to see your Self-Mastree", total_count: 0}
+Template.root.helpers({
+  root: function() {
+    if (Meteor.userId()) {
+      return Tree.findOne({root: true, uid: Meteor.userId()});
+    } else {
+      return {text: "Sign in to see your Self-Mastree", total_count: 0}
+    }
   }
-}
+});
 
 Template.root.rendered = function() {
 }
