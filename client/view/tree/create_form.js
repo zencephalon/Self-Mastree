ebbdd9c7ac9d1fbffdb2c7c28cbbea2bd2689e66
Matthew Bunday <zenceph@gmail.com>
2014-10-23 15:01:45 -0400

    Fix deprecation warnings with helpers setting.

--- client/view/tree/create_form.js
+++ client/view/tree/create_form.js
@@ -1,21 +1,23 @@
-Template.create_form.settings = function() {
-  return {
-    position: "bottom",
-    limit: 5,
-    rules: [{
-      token: '@',
-      replacement: '@(',
-      end_token: ') ',
-      collection: Trees,
-      filter: {archived: {"$ne": true}},
-      field: "ref",
-      matchAll: true,
-      template: Template.qs_tree_display,
-      callback: function(doc, element) {
-      }
-    }]
+Template.create_form.helpers({
+  settings: function() {
+    return {
+      position: "bottom",
+      limit: 5,
+      rules: [{
+        token: '@',
+        replacement: '@(',
+        end_token: ') ',
+        collection: Trees,
+        filter: {archived: {"$ne": true}},
+        field: "ref",
+        matchAll: true,
+        template: Template.qs_tree_display,
+        callback: function(doc, element) {
+        }
+      }]
+    }
   }
-};
+});
 
 Template.create_form.events({
   'submit': function (event) {
