e1e1f04d8eaf05a775a8b73a874e68ac4f5e3cb1
Matthew Bunday <zenceph@gmail.com>
2014-10-23 15:19:41 -0400

    Fix deprecation warnings.

--- client/view/quickswitcher/quickswitcher.js
+++ client/view/quickswitcher/quickswitcher.js
@@ -1,25 +1,27 @@
-Template.quickswitcher.settings = function() {
-  return {
-    position: "bottom",
-    limit: 5,
-    rules: [{
-      token: ':',
-      replacement: '',
-      end_token: '',
-      collection: Trees,
-      field: "ref",
-      matchAll: true,
-      filter: {archived: {"$ne": true}},
-      template: Template.qs_tree_display,
-      callback: function(doc, element) {
-        var tree = Tree.findOne(doc._id);
-        TreeView.focus.byId(tree._id);
-        tree.unfoldUp(true);
-        $('#quickswitcher').hide();
-      }
-    }]
+Template.quickswitcher.helpers({
+  settings: function() {
+    return {
+      position: "bottom",
+      limit: 5,
+      rules: [{
+        token: ':',
+        replacement: '',
+        end_token: '',
+        collection: Trees,
+        field: "ref",
+        matchAll: true,
+        filter: {archived: {"$ne": true}},
+        template: Template.qs_tree_display,
+        callback: function(doc, element) {
+          var tree = Tree.findOne(doc._id);
+          TreeView.focus.byId(tree._id);
+          tree.unfoldUp(true);
+          $('#quickswitcher').hide();
+        }
+      }]
+    }
   }
-}
+});
 
 Template.quickswitcher.rendered = function() {
   Mousetrap.bind('ctrl+space', function(e) {
